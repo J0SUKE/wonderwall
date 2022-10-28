618c297cdb4cbe7bb2bcd0921f0c87461d6472fd
Jean <chakib3500@gmail.com>
2022-10-28 12:28:29 +0200

    seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,28 @@
-import React, { useEffect } from 'react'
-import Home from '../components/pages/Home'
-import Layout from '../components/Layout'
-import { useRouter } from 'next/router'
-import { log } from 'console';
+import React, { useEffect } from "react";
+import Home from "../components/pages/Home";
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { log } from "console";
+import Head from "next/head";
 
 export default function HomePage() {
-  
   const router = useRouter();
 
-  useEffect(()=>{
-    router.events.on('routeChangeStart',()=>{
-      console.log('ready');
-      
-    })
-  })
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      console.log("ready");
+    });
+  });
 
-  return <Layout>
-    <Home/>
-  </Layout>
+  return (
+    <>
+      <Head>
+        <title>Wonderwall Studio – Beautifull Wallpapers</title>
+        <link rel="icon" href="/favicon.jpg" />
+      </Head>
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+  );
 }
-
