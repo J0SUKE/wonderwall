c44ce2f8636f87b37b48763d851ab393e74312c7
Jean <chakib3500@gmail.com>
2022-10-28 17:30:48 +0200

    load animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        show: 'show .6s forwards',
-      },
-      keyframes: {
-        show: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
